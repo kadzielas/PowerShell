5767c5b0a59882d2b3d8c5feb73636bc83987442
kadzielas <sebastian.kadziela@outlook.com>
2025-07-16 09:24:17 +0200

    did progress with error_handling lesson

--- Lessons/11_error_handling.ps1
+++ Lessons/11_error_handling.ps1
@@ -66,8 +66,14 @@ catch [System.Management.Automation.RuntimeException] {
 #Description: Try accessing C:\Windows\System32\config\SAM. Handle the error and write "Access Denied".
 
 
-
-
+<# 
+try {
+     Get-Content -Path C:\Windows\System32\config\SAM -ErrorAction Stop 
+}
+catch [System.IO.IOException] {
+     Write-Host "Error: $_"
+}
+#>
 
 
 #!------------------------------------------------------------------------------------------------------!
@@ -77,8 +83,14 @@ catch [System.Management.Automation.RuntimeException] {
 #Description: Create a script that always prints a message inside the finally block regardless of error.
 
 
-
-
+<# 
+try {
+     Get-Process | Select-Object -First 10
+}
+finally {
+     Write-Host "Activity complete"
+}
+ #>
 
 
 #!------------------------------------------------------------------------------------------------------!
@@ -96,9 +108,16 @@ catch [System.Management.Automation.RuntimeException] {
 #Description: Parse a user input using [int]::Parse() and handle FormatException and general errors differently.
 
 
-
-
-
+<# 
+try {
+     $output = Read-Host "Provide text"    
+     $intOutput = [int]::Parse($output)
+     $intOutput 
+}
+catch [System.FormatException] {
+     Write-Host "Error: $_"
+}
+ #>
 
 #!------------------------------------------------------------------------------------------------------!
 
@@ -107,7 +126,17 @@ catch [System.Management.Automation.RuntimeException] {
 #Description: Write a function that throws an error if input is negative. Use try/catch to handle it.
 
 
-
+<# 
+try {
+     [int]$output = Read-Host "Provide number above 0"
+     if (($output -lt 0)) {
+          Write-Error "Provided input is negative: $output" -ErrorAction stop
+     }
+}
+catch {
+     Write-Host "Error is negative: $_"
+}
+ #>
 
 
 #!------------------------------------------------------------------------------------------------------!
@@ -117,6 +146,26 @@ catch [System.Management.Automation.RuntimeException] {
 #Description: Try reading a missing file, and if it fails, try again after 2 seconds. Max 3 retries.
 
 
+<# 
+$isFile = $false
+
+try {
+     
+     if (Get-Content "C:/test/file.txt" -ErrorAction SilentlyContinue) {
+          Write-Host "Found"
+     } 
+     else {
+          for ($i = 1; $i -le 3; $i++) {
+               Start-Sleep -Seconds 2
+               Write-Host "$i# - trying to read file"
+               Get-Content "C:/test/file.txt" -ErrorAction SilentlyContinue
+          }
+     }
+}
+catch {
+     Write-Host "Error: $_"
+}
+ #>
 
 
 
@@ -127,7 +176,31 @@ catch [System.Management.Automation.RuntimeException] {
 #Description: Redirect error messages to a log file instead of displaying them on screen.
 
 
+<# 
+$isSuccess
 
+try {
+     if (!(Test-Path "C:\Raporty\11_4_logs.txt")) {
+          New-Item -ItemType File -Name "11_4_logs.txt" -Path "C:\Raporty\"
+     }
+     else {
+          Write-Host "11_4_logs.txt file has already exist"
+     }
+
+     for ($i = 0; $i -lt 5; $i++) {
+          if (Get-Content "C:/files/file$i.txt" -ErrorAction stop) {
+               $isSuccess = $true
+          }
+          else {
+               $isSuccess = $false
+          }
+     }
+}
+catch [System.Management.Automation.ItemNotFoundException] {
+     Add-Content -path "C:\Raporty\11_4_logs.txt" -Value "$_"
+
+}
+ #>
 
 
 #!------------------------------------------------------------------------------------------------------!
@@ -136,7 +209,18 @@ catch [System.Management.Automation.RuntimeException] {
 
 #Description: In the catch block, output the error's Exception.Message, CategoryInfo, and InvocationInfo.
 
-
+try {
+     Get-Process sadas -ErrorAction stop
+}
+catch {
+     Write-Host "|------------------------"
+     Write-Host "|Exception message: $($_.exception)"
+     Write-Host "|------------------------"
+     Write-Host "|CategoryInfo: $($_.CategoryInfo)"
+     Write-Host "|------------------------"
+     Write-Host "|InvocationInfo: $($_.InvocationInfo)"
+     Write-Host "|------------------------"
+}
 
 
 
